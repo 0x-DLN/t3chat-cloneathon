acb58038f0d26e2cbf8fdd475b62afdb108a6c6d
0xDLN <66573865+0x-DLN@users.noreply.github.com>
2025-06-16 00:57:32 +0200

    Send button zonder input

--- src/components/ai-chat.tsx
+++ src/components/ai-chat.tsx
@@ -2,21 +2,16 @@
 
 import { Authenticated } from "convex/react";
 import { BlockList } from "./block-list";
-import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { useTRPC } from "~/lib/trpc/react";
 
 export default function AiChat({ conversationId }: { conversationId: string }) {
   const trpc = useTRPC();
-  const [message, setMessage] = useState("");
 
   const sendMessage = useMutation(
     trpc.chat.sendBlockMessage.mutationOptions({
-      onSuccess: () => {
-        setMessage("");
-      },
+      onSuccess: () => {},
     })
   );
 
@@ -26,13 +21,7 @@ export default function AiChat({ conversationId }: { conversationId: string }) {
         <BlockList conversationId={conversationId} />
       </Authenticated>
       <div className="absolute bottom-10 w-full flex items-center justify-center">
-        <div className="flex items-center gap-2 w-full max-w-2xl">
-          {/* TODO: input in its own component so it doesn't cause blocks to rerender */}
-          <Input
-            className="w-full"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
+        <div className="flex items-center justify-center gap-2 w-full max-w-2xl">
           <Button
             onClick={() =>
               sendMessage.mutate({
@@ -42,7 +31,7 @@ export default function AiChat({ conversationId }: { conversationId: string }) {
               })
             }
           >
-            Send
+            Generate
           </Button>
         </div>
       </div>
