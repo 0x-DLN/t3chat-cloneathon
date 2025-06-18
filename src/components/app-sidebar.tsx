c25c33a467c39b9055b7c79d972fb792adb0d706
0xDLN <66573865+0x-DLN@users.noreply.github.com>
2025-06-18 20:02:50 +0200

    landing page sadge

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { Command, Github, Search } from "lucide-react";
+import { Github } from "lucide-react";
 import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import {
   Sidebar,
@@ -15,8 +14,6 @@ import {
   SidebarMenuItem,
 } from "~/components/ui/sidebar";
 import type { auth } from "~/lib/auth/server";
-import { Badge } from "./ui/badge";
-import { useEffect, useRef } from "react";
 import { Authenticated } from "convex/react";
 import ConversationsSidebar from "./conversations-sidebar";
 
@@ -25,41 +22,12 @@ type User = NonNullable<
 >["user"];
 
 export function AppSidebar({ user }: { user: User }) {
-  const searchRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault();
-        searchRef.current?.focus();
-      }
-    };
-
-    document.addEventListener("keydown", down);
-    return () => document.removeEventListener("keydown", down);
-  }, []);
-
   return (
     <Sidebar>
       <SidebarHeader>
         <Button className="w-full items-center justify-center" size="lg">
           New Chat
         </Button>
-
-        <div className="relative">
-          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-          <Input
-            placeholder="Search conversations..."
-            className="pl-10 pr-10"
-            ref={searchRef}
-          />
-          <Badge
-            variant="outline"
-            className="absolute right-2 top-1/2 transform -translate-y-1/2 rounded-none px-1 bg-accent text-accent-foreground"
-          >
-            <Command />K
-          </Badge>
-        </div>
       </SidebarHeader>
 
       <SidebarContent>
